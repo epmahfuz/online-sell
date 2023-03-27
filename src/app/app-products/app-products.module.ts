1d35c17c63a8bff075c8bf0255f3863e358200f4
al-mahfuz <al.mahfuz@selise.ch>
2023-03-27 09:59:49 +0600

    theaming update, product list

--- src/app/app-products/app-products.module.ts
+++ src/app/app-products/app-products.module.ts
@@ -6,6 +6,9 @@ import {AppCoreModule} from '../app-core/app-core.module';
 import { CategorySidebarComponent } from './components/category-sidebar/category-sidebar.component'; 
 import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NguCarouselModule } from '@ngu/carousel';
+import { TopCarouselComponent } from './components/top-carousel/top-carousel.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 const routes: Routes = [
   { path: '', component: AppHomeComponent },
 ]
@@ -13,13 +16,16 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppHomeComponent,
-    CategorySidebarComponent
+    CategorySidebarComponent,
+    TopCarouselComponent,
+    ProductListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MatIconModule,
     FlexLayoutModule,
+    NguCarouselModule,
     AppCoreModule
   ]
 })

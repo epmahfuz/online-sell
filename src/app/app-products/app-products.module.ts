c437b164f3055cd158cdbea5b08496295c09c12d
al-mahfuz <al.mahfuz@selise.ch>
2023-06-10 22:27:36 +0600

    mat form field sample added - common

--- src/app/app-products/app-products.module.ts
+++ src/app/app-products/app-products.module.ts
@@ -12,6 +12,12 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HighlightedCategoryComponent } from './components/highlighted-category/highlighted-category.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 const routes: Routes = [
   { path: '', component: AppHomeComponent },
   { path: 'checkout', component: CheckoutComponent },
@@ -33,7 +39,15 @@ const routes: Routes = [
     MatIconModule,
     FlexLayoutModule,
     NguCarouselModule,
-    AppCoreModule
+    AppCoreModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class AppProductsModule { }

f3a6eea206c85f01522d3c7644de37e66c0a564b
Md. Al Mahfuz <Al.Mahfuz@selise.ch>
2024-07-31 07:03:44 +0600

    footer and carousel - making global

--- src/app/app-core/app-core.module.ts
+++ src/app/app-core/app-core.module.ts
@@ -3,9 +3,12 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { SignInModalComponent } from './components/sign-in-modal/sign-in-modal.component';
 import { MaterialModule } from '../app-material/material.module';
 // import { MyAccountComponent } from './components/my-account/my-account.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NguCarouselModule } from '@ngu/carousel';
+import { TopCarouselComponent } from './components/top-carousel/top-carousel.component';
+import { CommonFooterComponent } from './components/common-footer/common-footer.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -15,6 +18,8 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [
     SearchBarComponent,
     SignInModalComponent,
+    TopCarouselComponent,
+    CommonFooterComponent,
     // MyAccountComponent,
   ], 
   imports: [
@@ -26,8 +31,9 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient],
       },
     }),
+    NguCarouselModule,
   ],
-  exports: [SearchBarComponent],
+  exports: [SearchBarComponent, TopCarouselComponent, CommonFooterComponent],
   entryComponents:[SignInModalComponent]
 })
 export class AppCoreModule { }
--- src/app/app-products/app-products.module.ts
+++ src/app/app-products/app-products.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AppHomeComponent } from './components/app-home/app-home.component';
 import { RouterModule, Routes } from '@angular/router';
-import {AppCoreModule} from '../app-core/app-core.module';
-import { CategorySidebarComponent } from './components/category-sidebar/category-sidebar.component'; 
-import { NguCarouselModule } from '@ngu/carousel';
-import { TopCarouselComponent } from './components/top-carousel/top-carousel.component';
+import { AppCoreModule } from '../app-core/app-core.module';
+import { CategorySidebarComponent } from './components/category-sidebar/category-sidebar.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HighlightedCategoryComponent } from './components/highlighted-category/highlighted-category.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { MyAccountComponent } from '../app-my-section/components/my-account/my-account.component';
 import { MaterialModule } from '../app-material/material.module';
-import { CommonFooterComponent } from './components/common-footer/common-footer.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'; 
 
@@ -28,17 +25,14 @@ const routes: Routes = [
   declarations: [
     AppHomeComponent,
     CategorySidebarComponent,
-    TopCarouselComponent,
     ProductListComponent,
     CartDetailsComponent,
     HighlightedCategoryComponent,
     CheckoutComponent,
     MyAccountComponent,
-    CommonFooterComponent
   ],
   imports: [
     RouterModule.forChild(routes),
-    NguCarouselModule,
     AppCoreModule,
     MaterialModule,
     TranslateModule.forRoot({
